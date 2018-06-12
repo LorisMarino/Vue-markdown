d6b2240ad842d1d303779e6aeba7938111276a02
LorisMarino <loris.marino@hetic.net>
2018-06-12 16:35:09 +0200

    :sparkles: Add localStorage sync

--- src/components/Editor/script.js
+++ src/components/Editor/script.js
@@ -7,9 +7,16 @@ export default {
     }
   },
 
+  mounted () {
+    if (localStorage.getItem('editorText')) {
+      this.editorText = localStorage.getItem('editorText')
+    }
+  },
+
   watch: {
     editorText () {
       this.$store.commit('editorText', this.editorText)
+      localStorage.setItem('editorText', this.editorText)
     }
   }
 }
