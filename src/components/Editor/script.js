06c0ac933d5702c5b5a256738c0d4a1171615484
LorisMarino <loris.marino@hetic.net>
2018-06-13 12:34:14 +0200

    :memo: Add pre-fill editor

--- src/components/Editor/script.js
+++ src/components/Editor/script.js
@@ -10,6 +10,9 @@ export default {
   mounted () {
     if (localStorage.getItem('editorText')) {
       this.editorText = localStorage.getItem('editorText')
+    } else {
+      const text = `# Markdown editor \n\n ## Bienvenue sur Mardown editor \n\n *__Écrivez__ votre markdown à gauche et __lisez-le__ à droite* \n\n **Retrouvez le code sur [GitHub](https://github.com/LorisMarino/Vue-markdown)**`
+      this.editorText = text
     }
   },
 
